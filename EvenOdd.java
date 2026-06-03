0f5985d7aad6c04df8586705df214ad5eb21ab8b
swarup-sawant <swarupsawant2005@gmail.com>
2026-06-03 08:42:11 +0530

    Added printEvenOdd Function

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,18 +1,20 @@
 import java.util.*;
-
 class EvenOdd{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter a number : ");
         int num = sc.nextInt();
-
-        if(num % 2 == 0){
-            System.out.println(num + " is a Even number..");
-        } else {
-            System.out.println(num + " it is Odd number..");
-        }
-
+        printEvenOdd(num);
         sc.close();
     }
+
+    public static void printEvenOdd(int num){
+        if(num % 2 == 0){
+                System.out.println(num + " is a Even number..");
+        } else {
+                System.out.println(num + " it is Odd number..");
+        }
+    }
+
 }
